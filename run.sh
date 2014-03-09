08aec6dfa7e7f5b1fd320d4d22ca7e0d16d1ba43
Florian Brinker <mail@develign.net>
2014-03-09 17:55:10 +0100

    Init

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install unzip
+
+cd $WERCKER_ROOT
+debug 'Downloading AppEngine SDK...'
+
+curl -O https://commondatastorage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.0.zip ; unzip -x google_appengine_1.9.0.zip
+export PATH="$PATH:$(pwd)/go_appengine"
+
+echo "$WERCKER_APPENGINE_DEPLOY_PATH_PASSWORD" > "$WERCKER_STEP_TEMP/password"
+
+cd $WERCKER_SOURCE_DIR
+
+debug 'Starting deployment of directory'
+appcfg.py update $WERCKER_APPENGINE_DEPLOY_PATH_SRCPATH --email="$WERCKER_APPENGINE_DEPLOY_PATH_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
+
+success 'Finished'
