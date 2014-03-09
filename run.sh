0a9e933a77911f1e43e7fb7b97d258abf6ef5f4a
Florian Brinker <mail@develign.net>
2014-03-09 18:32:26 +0100

    Pathfix

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd $WERCKER_ROOT
 debug 'Downloading AppEngine SDK...'
 
 curl -O https://commondatastorage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.0.zip ; unzip -x google_appengine_1.9.0.zip
-export PATH="$PATH:$(pwd)/go_appengine"
+export PATH="$PATH:$(pwd)/google_appengine"
 
 echo "$WERCKER_APPENGINE_DEPLOY_PATH_PASSWORD" > "$WERCKER_STEP_TEMP/password"
 
