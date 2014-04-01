e1f672cad96aad2e2bdcc2cfd22a30dc602d90b3
mattetre <etre.matthew@gmail.com>
2014-03-31 22:29:04 -0400

    fix var names

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ debug 'Downloading AppEngine SDK...'
 curl -O https://commondatastorage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.1.zip ; unzip -x google_appengine_1.9.1.zip
 export PATH="$PATH:$(pwd)/google_appengine"
 
-echo "$WERCKER_APPENGINE_DEPLOY_PATH_PASSWORD" > "$WERCKER_STEP_TEMP/password"
+echo "$WERCKER_APPENGINE_DEPLOY_PATH_UPDATE_PASSWORD" > "$WERCKER_STEP_TEMP/password"
 
 cd $WERCKER_SOURCE_DIR
 
 debug 'Starting deployment of directory'
-echo $WERCKER_APPENGINE_DEPLOY_PATH_SRCPATH
-echo $WERCKER_APPENGINE_DEPLOY_PATH_EMAIL
-appcfg.py update $WERCKER_APPENGINE_DEPLOY_PATH_SRCPATH --email="$WERCKER_APPENGINE_DEPLOY_PATH_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
+echo $WERCKER_APPENGINE_DEPLOY_PATH_UPDATE_SRCPATH
+echo $WERCKER_APPENGINE_DEPLOY_PATH_UPDATE_EMAIL
+appcfg.py update $WERCKER_APPENGINE_DEPLOY_PATH_UPDATE_SRCPATH --email="$WERCKER_APPENGINE_DEPLOY_PATH_UPDATE_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
 
 success 'Finished'
