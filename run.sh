1b9d4fa243cfa9ed72f7adda7729745fc04d8c01
Matt Etre <etre.matthew@gmail.com>
2014-05-30 16:46:04 -0400

    bump versions

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo apt-get install unzip
 cd $WERCKER_ROOT
 debug 'Downloading AppEngine SDK...'
 
-curl -o appengine.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.4.zip; unzip -x appengine.zip
+curl -o appengine.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.5.zip; unzip -x appengine.zip
 export PATH="$PATH:$(pwd)/google_appengine"
 
 echo "$WERCKER_APPENGINE_DEPLOY_PATH_UPDATE_PASSWORD" > "$WERCKER_STEP_TEMP/password"
